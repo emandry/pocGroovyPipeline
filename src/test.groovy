42fbe4979e72a61675aefc12f1986d7bf3812ca5
emandry <30294910+emandry@users.noreply.github.com>
2017-08-04 16:56:50 -0400

    Added more code into de groovy file

--- src/test.groovy
+++ src/test.groovy
@@ -1,1 +1,24 @@
 println "HELLO EVERYONE"
+
+BUILD_JOB_NAME = "My Multibranch Java Project/development"
+def getJob() {
+    def hi = Hudson.instance
+    return hi.getItemByFullName(BUILD_JOB_NAME)
+}
+def getBuildGitInfo(Integer buildNumber) {
+    def gitInfo = "" 
+    println "INSIDE"
+    
+    def buildJob = getJob()
+    
+    buildJob.getBuildByNumber(buildNumber).getChangeSets().each { elem->
+        elem.each { datos->
+
+            def affectedPathsData = datos.getAffectedPaths().join()
+            gitInfo = gitInfo + "Commit ID(Revision): " + datos.getCommitId() + "\nCommit Message: " + datos.getMsg() + "\nAffected Path: " + affectedPathsData + "\nAuthor: " + datos.getAuthor() + "\n"
+        }
+    }
+    return gitInfo
+}
+
+print getBuildGitInfo(46)
